07cee25a116c06540a8de907e537bded9f0d3f02
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2024-09-02 14:19:50 +0700

    first init

---
+++ components/inputComponent.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Text, Button, View, StyleSheet, TextInput } from "react-native";
+
+const inputComponent = (a,b) => {
+    let names;
+    const [InputA, setInputA] = useState(null);
+
+}
+
+const outputComponent = () => {
+    const A_dari_sebelah = InputA;
+    let names; 
+}
+
+
+
+const style = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 4,
+        margin: 4,
+    }
+});
